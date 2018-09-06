58db70466f2371346c744535c2cf612f4b3feb89
mick.yi <mick.yi@embracesource.com>
2018-09-06 22:05:06 +0800

    增加RmsProp

--- nn/optimizers.py
+++ nn/optimizers.py
@@ -59,7 +59,7 @@ class SGD(object):
 
 
 class AdaGrad(object):
-    def __init__(self, weights, lr=0.01, epsilon=1e-6, decay=1e-5):
+    def __init__(self, weights, lr=0.01, epsilon=1e-6, decay=0):
         """
 
         :param weights: 权重
@@ -86,6 +86,45 @@ class AdaGrad(object):
         # 更新权重平方和累加量 和 梯度
         for key in self.s.keys():
             self.s[key] += np.square(weights[key])
+            weights[key] -= self.lr * gradients[key] / np.sqrt(self.s[key] + self.epsilon)
+
+        # 更新迭代次数
+        self.iterations += 1
+
+        # 返回梯度
+        return weights
+
+
+class RmsProp(object):
+    def __init__(self, weights, gamma=0.9, lr=0.01, epsilon=1e-6, decay=0):
+        """
+
+        :param weights: 权重
+        :param gamma: 指数
+        :param lr: 学习率
+        :param epsilon: 平滑数
+        :param decay: 学习率衰减
+        """
+        self.s = _copy_weights_to_zeros(weights)  # 权重平方和累加量
+        self.gamma = gamma
+        self.iterations = 0  # 迭代次数
+        self.lr = self.init_lr = lr
+        self.epsilon = epsilon
+        self.decay = decay
+
+    def iterate(self, weights, gradients):
+        """
+        迭代一次
+        :param weights: 当前迭代权重
+        :param gradients: 当前迭代梯度
+        :return:
+        """
+        # 更新学习率
+        self.lr = self.init_lr / (1 + self.iterations)
+
+        # 更新权重平方和累加量 和 梯度
+        for key in self.s.keys():
+            self.s[key] = self.gamma * self.s[key] + (1 - self.gamma) * np.square(weights[key])
             weights[key] -= self.lr * gradients[key] / np.sqrt(self.s[key] + self.epsilon)
 
         # 更新迭代次数
