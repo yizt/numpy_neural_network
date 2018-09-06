f0589cdc060ec71c19b0c1182cf1790be712678c
mick.yi <mick.yi@embracesource.com>
2018-09-06 21:55:54 +0800

    增加AdaGrad

--- nn/optimizers.py
+++ nn/optimizers.py
@@ -10,6 +10,14 @@ Created on 2018/9/4 22:26
 import numpy as np
 
 
+def _copy_weights_to_zeros(self, weights):
+    result = {}
+    result.keys()
+    for key in weights.keys():
+        result[key] = np.zeros_like(weights[key])
+    return result
+
+
 class SGD(object):
     """
     小批量梯度下降法
@@ -23,22 +31,21 @@ class SGD(object):
         :param momentum: 动量
         :param decay: 学习率衰减
         """
-        self.v = self._copy_weights_to_zeros(weights)
+        self.v = _copy_weights_to_zeros(weights)
         self.iterations = 0  # 迭代次数
-        self.lr = lr
+        self.lr = self.init_lr = lr
         self.momentum = momentum
         self.decay = decay
 
-    def _copy_weights_to_zeros(self, weights):
-        result = {}
-        result.keys()
-        for key in weights.keys():
-            result[key] = np.zeros_like(weights[key])
-        return result
-
-    def iterator(self, weights, gradients):
+    def iterate(self, weights, gradients):
+        """
+        迭代一次
+        :param weights: 当前迭代权重
+        :param gradients: 当前迭代梯度
+        :return:
+        """
         # 更新学习率
-        self.lr /= (1 + self.iterations)
+        self.lr = self.init_lr / (1 + self.iterations)
         # 更新动量和梯度
         for key in self.v.keys():
             self.v[key] = self.momentum * self.v[key] + self.lr * gradients[key]
@@ -48,4 +55,41 @@ class SGD(object):
         self.iterations += 1
 
         # 返回更新后的权重
+        return weights
+
+
+class AdaGrad(object):
+    def __init__(self, weights, lr=0.01, epsilon=1e-6, decay=1e-5):
+        """
+
+        :param weights: 权重
+        :param lr: 学习率
+        :param epsilon: 平滑数
+        :param decay: 学习率衰减
+        """
+        self.s = _copy_weights_to_zeros(weights)  # 权重平方和累加量
+        self.iterations = 0  # 迭代次数
+        self.lr = self.init_lr = lr
+        self.epsilon = epsilon
+        self.decay = decay
+
+    def iterate(self, weights, gradients):
+        """
+        迭代一次
+        :param weights: 当前迭代权重
+        :param gradients: 当前迭代梯度
+        :return:
+        """
+        # 更新学习率
+        self.lr = self.init_lr / (1 + self.iterations)
+
+        # 更新权重平方和累加量 和 梯度
+        for key in self.s.keys():
+            self.s[key] += np.square(weights[key])
+            weights[key] -= self.lr * gradients[key] / np.sqrt(self.s[key] + self.epsilon)
+
+        # 更新迭代次数
+        self.iterations += 1
+
+        # 返回梯度
         return weights
