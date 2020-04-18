fc11bf007c9f0ac78e03b2a1b2f96d1dbc56017f
yizt <csuyzt@163.com>
2020-04-18 11:06:27 +0800

    add vgg

---
+++ nn/vgg.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+ @File    : vgg.py
+ @Time    : 2020/4/18 上午11:01
+ @Author  : yizuotian
+ @Description    :
+"""
+from .modules import *
+
+
+class VGG(Model):
+    """
+    VGG 模型
+    """
