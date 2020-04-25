06bea52ffbe6183d31c9e2c0ed0b38af174310da
yizt <csuyzt@163.com>
2020-04-25 10:32:41 +0800

    效率更高的卷积

--- nn/layers_v2.py
+++ nn/layers_v2.py
@@ -51,6 +51,62 @@ def _single_channel_conv(z, K, b=0, padding=(0, 0)):
     return conv_z + b
 
 
+def conv_forward_v1(z, K, b, padding=(0, 0)):
+    """
+    多通道卷积前向过程
+    :param z: 卷积层矩阵,形状(N,C,H,W)，N为batch_size，C为通道数
+    :param K: 卷积核,形状(C,D,k1,k2), C为输入通道数，D为输出通道数
+    :param b: 偏置,形状(D,)
+    :param padding: padding
+    :return: 卷积结果
+    """
+    padding_z = np.lib.pad(z, ((0, 0), (0, 0), (padding[0], padding[0]), (padding[1], padding[1])), 'constant',
+                           constant_values=0)
+    N, _, height, width = padding_z.shape
+    C, D, k1, k2 = K.shape
+    oh, ow = (1 + (height - k1), 1 + (width - k2))  # 输出的高度和宽度
+    conv_z = np.zeros((N, D, oh, ow))
+    for n in np.arange(N):
+        for d in np.arange(D):
+            for h in np.arange(oh):
+                for w in np.arange(oh):
+                    conv_z[n, d, h, w] = np.sum(
+                        padding_z[n, :, h:h + k1, w:w + k2] * K[:, d]) + b[d]
+    return conv_z
+
+
+def _conv_forward(z, K, b, padding=(0, 0)):
+    """
+    多通道卷积前向过程
+    :param z: 卷积层矩阵,形状(N,C,H,W)，N为batch_size，C为通道数
+    :param K: 卷积核,形状(C,D,k1,k2), C为输入通道数，D为输出通道数
+    :param b: 偏置,形状(D,)
+    :param padding: padding
+    :return: 卷积结果
+    """
+    padding_z = np.lib.pad(z, ((0, 0), (0, 0), (padding[0], padding[0]), (padding[1], padding[1])), 'constant',
+                           constant_values=0)
+    N, _, height, width = padding_z.shape
+    C, D, k1, k2 = K.shape
+    oh, ow = (1 + (height - k1), 1 + (width - k2))  # 输出的高度和宽度
+
+    # 扩维
+    padding_z = padding_z[:, :, np.newaxis, :, :]  # 扩维[N,C,1,H,W] 与K [C,D,K1,K2] 可以广播
+    K = K[:, :, :, :, np.newaxis, np.newaxis]
+    conv_z = np.zeros((N, C, D, oh, ow))
+
+    # 批量卷积
+    for i in range(k1):
+        for j in range(k2):
+            # [N,C,1,oh,ow]*[C,D,1,1] =>[N,C,D,oh,ow]
+            conv_z += padding_z[:, :, :, i:i + oh, j:j + ow] * K[:, :, i, j]
+
+    conv_z = np.sum(conv_z, axis=1)  # [N, C, D, oh, ow] => [N, D, oh, ow]
+    # 增加偏置 [N, D, oh, ow]+[D, 1, 1]
+    conv_z += b[:, np.newaxis, np.newaxis]
+    return conv_z
+
+
 def test_single_conv():
     """
     两个卷积结果一样，速度相差百倍以上
@@ -69,5 +125,25 @@ def test_single_conv():
     print(np.allclose(o1, o2))
 
 
+def test_conv():
+    """
+    两个卷积结果一样，速度相差几十倍
+    :return:
+    """
+    z = np.random.randn(4, 64, 224, 224)
+    K = np.random.randn(64, 64, 3, 3)
+    b = np.random.randn(64)
+
+    s = time.time()
+    o1 = conv_forward_v1(z, K, b)
+    print("v1 耗时:{}".format(time.time() - s))
+    s = time.time()
+    o2 = _conv_forward(z, K, b)
+    print("v2 耗时:{}".format(time.time() - s))
+
+    print(np.allclose(o1, o2))
+
+
 if __name__ == '__main__':
-    test_single_conv()
+    # test_single_conv()
+    test_conv()
