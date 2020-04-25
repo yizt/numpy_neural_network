bf336378b8d35c6a2b757ca5a12c2f3f6b1fd61f
yizt <csuyzt@163.com>
2020-04-25 19:23:22 +0800

    更新setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
+import numpy as np
 import setuptools
 from setuptools.extension import Extension
-import numpy as np
 
 extensions = [
     Extension(
@@ -8,17 +8,22 @@ extensions = [
         ['nn/clayers.pyx'],
         include_dirs=[np.get_include()]
     ),
+    Extension(
+        'nn.clayers_v2',
+        ['nn/clayers_v2.pyx'],
+        include_dirs=[np.get_include()]
+    )
 ]
 
 setuptools.setup(
-    name             = 'numpy_neuron_network',
-    version          = '0.1',
-    description      = 'numpy 构建神经网络',
-    url              = 'https://github.com/yizt/numpy_neuron_network',
-    author           = 'yizt',
-    author_email     = 'csuyzt@163.com',
-    packages         = setuptools.find_packages(),
-    install_requires = ['numpy', 'cython'],
-    ext_modules    = extensions,
-    setup_requires = ["cython>=0.28"]
+    name='numpy_neuron_network',
+    version='0.1',
+    description='numpy 构建神经网络',
+    url='https://github.com/yizt/numpy_neuron_network',
+    author='yizt',
+    author_email='csuyzt@163.com',
+    packages=setuptools.find_packages(),
+    install_requires=['numpy', 'cython'],
+    ext_modules=extensions,
+    setup_requires=["cython>=0.28"]
 )
